837d3a7b201f0fd950c41265b597f0ffc9b8183a
Selim Bucher <selim.bucher@slimmer.ch>
2026-03-04 23:28:05 +0100

    chore: exclude ouputs/build from git

--- outputs/out/share/widget/filterAppIcons.tsx
+++
@@ -1,8 +0,0 @@
-export default function filterApp(name: string): string {
-    switch (name) {
-        case "code":
-            return "vscode"
-        default:
-            return name
-    }
-}
