e1463b1459cd237a843012d6892d5cb56275d802
Selim Bucher <selim.bucher@slimmer.ch>
2026-03-09 19:53:49 +0100

    fix: uncomment "import AppSwitcher" si it can be used

--- src/desktop/app.ts
+++ src/desktop/app.ts
@@ -2,7 +2,7 @@ import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widgets/Bar/Bar"
 import IndicatorBar, { showIndicator } from "./widgets/IndicatorBar/IndicatorBar"
-// import AppSwitcher, { toggleAppSwitcher } from "./widget/AppSwitcher/AppSwitcher"
+import AppSwitcher, { toggleAppSwitcher } from "./widgets/AppSwitcher/AppSwitcher"
 import Dock, { EdgeSensor } from "./widgets/Dock/Dock"
 import { Gtk } from "ags/gtk4"
 
@@ -30,7 +30,7 @@ app.start({
   main() {
     app.get_monitors().map(Bar)
     app.get_monitors().map(IndicatorBar)
-    // app.get_monitors().map(AppSwitcher)
+    app.get_monitors().map(AppSwitcher)
     app.get_monitors().map(Dock)
     app.get_monitors().map(EdgeSensor)
     
