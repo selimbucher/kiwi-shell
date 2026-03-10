ba2f10c22f04b080a16b4aad6c8bb32916ab5d91
Selim Bucher <selim.bucher@slimmer.ch>
2026-03-10 04:16:22 +0100

    chore: include the requirement checks into app.ts

--- src/kiwi-shell/app.ts
+++ src/kiwi-shell/app.ts
@@ -1,3 +1,6 @@
+import { checkRequirements } from "./widgets/requirements"
+checkRequirements()
+
 import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widgets/Bar/Bar"
